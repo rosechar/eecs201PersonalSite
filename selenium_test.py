a20cb740a6f3ff4c92041f5106630e0401993df9
rosechar <rosechar@umich.edu>
2021-03-02 16:32:37 -0500

    A

--- selenium_test.py
+++ selenium_test.py
@@ -6,5 +6,6 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://rosechar.github.io/updates")
 updates = driver.find_elements(By.ID, 'update')
+find_elements_by_link_text
 print(updates)
 driver.close()
