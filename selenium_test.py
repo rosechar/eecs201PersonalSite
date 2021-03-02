4c82babf46b6c162d1b1146eee59ab573a385c53
rosechar <rosechar@umich.edu>
2021-03-02 16:56:35 -0500

    A

--- selenium_test.py
+++ selenium_test.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 
 
 driver = webdriver.Chrome()
 driver.get("https://rosechar.github.io/updates")
 updates = driver.find_elements(By.ID, 'update')
-find_elements_by_link_text
-print(updates)
+links = driver.find_elements_by_link_text('read...')
+for update in updates:
+    print(update.text.split('\n')[0])
+for link in links:
+    url = link.get_attribute('href')
+    print(url)
+    driver2 = webdriver.Chrome()
+    driver2.get(url)
+    if ("404" in driver2.page_source):
+        print('this url is invalid:' + url)
+        exit(1)
+    driver2.close()
 driver.close()
