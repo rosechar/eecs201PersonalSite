f5dc5223637de14b4e02ea731a1f0d4ac121dbae
rosechar <rosechar@umich.edu>
2021-03-02 20:08:30 -0500

    new HTML file and updated updates.html

--- selenium_test.py
+++ selenium_test.py
@@ -17,6 +17,7 @@ for link in links:
     if ("404" in driver2.page_source):
         print('this url is invalid:' + url)
         driver2.close()
+        driver.close()
         exit(1)
     driver2.close()
 driver.close()
